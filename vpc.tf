66647eb563cad1b3e8b2b1e297949e97883b55b0
jnunortiz <jnunortiz@gmail.com>
2025-02-20 23:46:35 +0100

    Configuration of ingress firewall rules for subnet allowing SSH and HTTP connections

--- vpc.tf
+++ vpc.tf
@@ -11,3 +11,31 @@ resource "google_compute_subnetwork" "my_subnet" {
   region        = var.region
   network       = google_compute_network.my_vpc.id
 }
+
+# Resource: Firewall Rule SSH
+resource "google_compute_firewall" "fw_ssh" {
+  name    = "fw-ssh22"
+  network = google_compute_network.my_vpc.id
+  direction = "INGRESS"
+  priority = 1000
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+  source_ranges = ["0.0.0.0/0"]
+  target_tags = ["allow-ssh"]
+}
+
+# Resource: Firewall Rule HTTP
+resource "google_compute_firewall" "fw_http" {
+  name    = "fw-http80"
+  network = google_compute_network.my_vpc.id
+  direction = "INGRESS"
+  priority = 1000
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+  source_ranges = ["0.0.0.0/0"]
+  target_tags = ["allow-http"]
+}
