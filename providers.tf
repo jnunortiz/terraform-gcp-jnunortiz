404c88542feadf7ab881ac8d3438c7580cbc7b0e
jnunortiz <jnunortiz@gmail.com>
2025-02-24 23:24:40 +0100

    Add Google provider configurations for Europe regions

---
+++ providers.tf
@@ -0,0 +1,15 @@
+provider "google" {
+  project = var.project_id
+  region  = var.europe_west1
+  zone = var.europe_west1_a
+  billing_project = "terraform-gcp-jnunortiz"
+  alias = "europe_west1"
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.europe_west3
+  zone = var.europe_west3_a
+  billing_project = "terraform-gcp-jnunortiz"
+  alias = "europe_west3"
+}
