99b5ce35727a5b712091b7d5535ef94ee0e94825
jnunortiz <jnunortiz@gmail.com>
2025-02-25 22:22:14 +0100

    Remove default values from Terraform variables for project ID, regions, zones, and machine type

--- variables.tf
+++ variables.tf
@@ -1,35 +1,29 @@
 variable "project_id" {
   description = "The ID of the project in which to provision resources."
   type        = string
-  default     = "terraform-gcp-jnunortiz"
 }
 
 variable "europe_west1" {
   description = "The region in which to provision resources."
   type        = string
-  default     = "europe-west1"
 }
 
 variable "europe_west1_a" {
   description = "The zone in which to provision resources."
   type        = string
-  default     = "europe-west1-a"
 }
 
 variable "europe_west3" {
   description = "The region in which to provision resources."
   type        = string
-  default     = "europe-west3"
 }
 
 variable "europe_west3_a" {
   description = "The zone in which to provision resources."
   type        = string
-  default     = "europe-west3-a"
 }
 
 variable "machine_type" {
   description = "The machine type to use for the compute instance."
   type        = string
-  default     = "e2-micro"
 }
