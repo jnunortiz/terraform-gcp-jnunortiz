a22a6a00034bed55c27dea3101678b54da15fcae
jnunortiz <jnunortiz@gmail.com>
2025-02-25 22:48:36 +0100

    Add outputs for VM instance ID and external IP address in outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,9 @@
+output "vm_instance_id" {
+  description = "The ID of the Compute Engine instance."
+  value       = google_compute_instance.vm_1.id
+}
+
+output "vm_external_ip" {
+  description = "The external IP address of the Compute Engine instance."
+  value       = google_compute_instance.vm_1.network_interface.0.access_config.0.assigned_nat_ip
+}
