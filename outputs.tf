b4b710442b56ddd7d703af7fe21326ba38a5be3b
jnunortiz <jnunortiz@gmail.com>
2025-02-25 22:49:51 +0100

    Fix output value for external IP address in outputs.tf to use correct indexing

--- outputs.tf
+++ outputs.tf
@@ -5,5 +5,5 @@ output "vm_instance_id" {
 
 output "vm_external_ip" {
   description = "The external IP address of the Compute Engine instance."
-  value       = google_compute_instance.vm_1.network_interface.0.access_config.0.assigned_nat_ip
+  value       = google_compute_instance.vm_1.network_interface[0].access_config[0].nat_ip
 }
