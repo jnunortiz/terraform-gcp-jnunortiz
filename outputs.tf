dff6360ac557431ca594e58aafd6f5c3886b2de1
jnunortiz <jnunortiz@gmail.com>
2025-02-25 22:52:17 +0100

    Add additional outputs for VM instance details in outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,18 @@ output "vm_external_ip" {
   description = "The external IP address of the Compute Engine instance."
   value       = google_compute_instance.vm_1.network_interface[0].access_config[0].nat_ip
 }
+
+output "vm_self_link" {
+  description = "The self link of the Compute Engine instance."
+  value       = google_compute_instance.vm_1.self_link
+}
+
+output "vm_machine_type" {
+  description = "The machine type of the Compute Engine instance."
+  value       = google_compute_instance.vm_1.machine_type
+}
+
+output "vm_machine_name" {
+  description = "The name of the Compute Engine instance."
+  value       = google_compute_instance.vm_1.name
+}
