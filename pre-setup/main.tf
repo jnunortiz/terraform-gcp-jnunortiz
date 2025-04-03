3d93168ff3197a78c67d5f0cf19226e960024425
jnunortiz <jnunortiz@gmail.com>
2025-04-03 22:56:35 +0200

    Update IAM role from compute.admin to compute.instanceAdmin.v1 for service account permissions

--- pre-setup/main.tf
+++ pre-setup/main.tf
@@ -16,7 +16,7 @@ resource "google_project_iam_member" "service_account_roles" {
     "roles/iam.serviceAccountTokenCreator",
     "roles/iam.serviceAccountUser",
     "roles/resourcemanager.projectIamAdmin",
-    "roles/compute.admin"
+    "roles/compute.instanceAdmin.v1"
   ])
   project = var.project_id
   role    = each.value
